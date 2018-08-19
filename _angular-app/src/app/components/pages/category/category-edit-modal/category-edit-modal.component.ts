9ff4bf0eb148c0af7720b0840d1a1d42ed967952
Daniel Dauber <ddauberox@gmail.com>
2018-08-18 22:38:23 -0300

    crud front categorias

---
+++ _angular-app/src/app/components/pages/category/category-edit-modal/category-edit-modal.component.ts
@@ -0,0 +1,76 @@
+import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {ModalComponent} from "../../../bootstrap/modal/modal.component";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+
+@Component({
+  selector: 'category-edit-modal',
+  templateUrl: './category-edit-modal.component.html',
+  styleUrls: ['./category-edit-modal.component.css']
+})
+export class CategoryEditModalComponent implements OnInit {
+
+  category = {
+    name: '',
+    active: true
+  };
+
+  _categoryId: number;
+
+  @ViewChild(ModalComponent) modal: ModalComponent;
+  @Output() onSuccess: EventEmitter<any> = new EventEmitter<any>();
+  @Output() onError: EventEmitter<HttpErrorResponse> = new EventEmitter<HttpErrorResponse>();
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+  }
+
+  @Input()
+  set categoryId(value) {
+    this._categoryId = value;
+    if (this._categoryId) {
+      const token = window.localStorage.getItem('token');
+
+      this.http.get<{ data: any }>(`http://server.local:8000/api/categories/${value}`, {
+        headers: {
+          'Authorization': `Bearer ${token}`
+
+        }
+      })
+        .subscribe((response) => this.category = response.data);
+
+    }
+
+  }
+
+  submit() {
+
+    const token = window.localStorage.getItem('token');
+
+    this.http
+      .put(`http://server.local:8000/api/categories/${this._categoryId}`, this.category, {
+        headers: {
+          'Authorization': `Bearer ${token}`
+
+        }
+      })
+      .subscribe((category) => {
+        this.onSuccess.emit(category);
+        this.modal.hide();
+        //this.getCategories();
+      }, error => this.onError.emit(error));
+
+  }
+
+
+  showModal() {
+    this.modal.show()
+  }
+
+  hideModal($event) {
+    console.log($event)
+
+  }
+
+}
--- _angular-app/src/app/components/pages/category/category-list/category-list.component.ts
+++ _angular-app/src/app/components/pages/category/category-list/category-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import { CategoryNewModalComponent } from '../category-new-modal/category-new-modal.component'
+import {CategoryEditModalComponent} from "../category-edit-modal/category-edit-modal.component";
 
 @Component({
   selector: 'app-category-list',
@@ -10,14 +12,27 @@ export class CategoryListComponent implements OnInit {
 
   categories : Array<{id: number,name: string, active: boolean, created_at: {date: string}}> = [];
 
+  @ViewChild(CategoryNewModalComponent)
+  categoryNewModal: CategoryNewModalComponent;
+
+  @ViewChild(CategoryEditModalComponent)
+  categoryEditModal: CategoryEditModalComponent;
+
+  categoryId : number;
+
+
   constructor(private http : HttpClient) { }
 
   ngOnInit() {
 
+   this.getCategories()
+  }
+
+  getCategories() {
     const token = window.localStorage.getItem('token');
 
     this.http.get<{data: Array<{id: number,name: string, active: boolean, created_at: {date: string}}>}
-    >('http://server.local:8000/api/categories', {
+      >('http://server.local:8000/api/categories', {
       headers: {
         'Authorization' : `Bearer ${token}`
 
@@ -25,5 +40,39 @@ export class CategoryListComponent implements OnInit {
     })
       .subscribe(response => this.categories = response.data);
   }
+
+  showModalInsert(){
+    this.categoryNewModal.showModal();
+
+  }
+
+  showModalEdit(categoryId: number){
+    this.categoryId = categoryId;
+    this.categoryEditModal.showModal();
+  }
+
+
+  onInsertSuccess($event : any){
+    this.getCategories()
+    console.log($event)
+
+  }
+
+  onInsertError($event: HttpErrorResponse) {
+    console.log($event)
+
+  }
+
+  onEditSuccess($event : any){
+    this.getCategories()
+    console.log($event)
+
+  }
+
+  onEditError($event: HttpErrorResponse) {
+    console.log($event)
+
+  }
+
 
 }
